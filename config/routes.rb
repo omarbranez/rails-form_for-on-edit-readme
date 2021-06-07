b15655a1b11740c947dda2901ca4063cccecf8c8
Omar Branez <omar.branez@gmail.com>
2021-06-07 11:13:04 -0400

    commit tree 45d25d30035654396a312deca3e7c88caa87794c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # define dynamic route as PATCH instead of PUT
+  # PUT updates the entire object, PATCH only updates the elements changed
+  patch 'posts/:id', to: 'posts#update'
 end
